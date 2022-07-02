394e4e76fe59b7a4b4ac7a0b4a7b802a14833096
Julian Brust <j.brust@techtriq.com>
2022-07-02 21:01:15 +0200

    update logger setup

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gdamore/tcell/v2 v2.5.1
+	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/text v0.3.7
 	gopkg.in/yaml.v3 v3.0.1
 )
--- main.go
+++ main.go
@@ -4,27 +4,30 @@ import (
 	"github.com/julianbrust/media-browser/browser/shows"
 	"github.com/julianbrust/media-browser/config"
 	"github.com/julianbrust/media-browser/tmdb"
-	"log"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
-func main() {
-	//TODO: clean up logging
+var log *logrus.Logger
+
+func init() {
 	file := "/tmp/media-browser.log"
+	log = logrus.New()
 
 	err := os.Remove(file)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	logFile, err := os.OpenFile(file, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
-	defer logFile.Close()
 
 	log.SetOutput(logFile)
+}
 
+func main() {
 	conf, err := config.ReadConf()
 	if err != nil {
 		log.Fatal(err)
